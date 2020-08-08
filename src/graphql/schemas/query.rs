728f43ef55fa213a9856a96baab4c94e773cbc95
John <john.zhao@scrumcn.com>
2020-08-08 12:58:48 +0800

    测试

--- src/graphql/schemas/query.rs
+++ src/graphql/schemas/query.rs
@@ -1,23 +1,6 @@
 
-use async_graphql::{Deferred, Object};
-use std::time::Duration;
+use async_graphql::{Object};
 
-struct Comment {
-    user: String,
-    text: String,
-}
-
-#[Object]
-impl Comment {
-    async fn user(&self) -> &str {
-        actix_rt::time::delay_for(Duration::from_secs(2)).await;
-        &self.user
-    }
-
-    async fn text(&self) -> &str {
-        &self.text
-    }
-}
 
 struct Book {
     id: i32,
@@ -27,42 +10,15 @@ struct Book {
 
 #[Object]
 impl Book {
+    async fn id(&self) -> i32 {
+        self.id
+    }
     async fn title(&self) -> &str {
         &self.title
     }
 
     async fn author(&self) -> &str {
         &self.author
-    }
-
-    async fn comments(&self) -> Deferred<Vec<Comment>> {
-        if self.id == 1 {
-            vec![
-                Comment {
-                    user: "John".to_string(),
-                    text: "I liked it".to_string(),
-                },
-                Comment {
-                    user: "Mary".to_string(),
-                    text: "It is a book".to_string(),
-                },
-            ]
-            .into()
-        } else if self.id == 2 {
-            vec![
-                Comment {
-                    user: "Alberta".to_string(),
-                    text: "Amazing :-)".to_string(),
-                },
-                Comment {
-                    user: "Joanna".to_string(),
-                    text: "Excellent".to_string(),
-                },
-            ]
-            .into()
-        } else {
-            Vec::new().into()
-        }
     }
 }
 
@@ -74,18 +30,18 @@ impl Query {
         vec![
             Book {
                 id: 1,
-                title: "Harry Potter and the Chamber of Secrets".to_string(),
-                author: "J.K. Rowling".to_string(),
+                title: "踢啊四四四口大口大口大口".to_string(),
+                author: "John".to_string(),
             },
             Book {
                 id: 2,
-                title: "Jurassic Park".to_string(),
-                author: "Michael Crichton".to_string(),
+                title: "上课送我我哦饿哦饿".to_string(),
+                author: "Jobin".to_string(),
             },
             Book {
                 id: 3,
-                title: "Moby Dick".to_string(),
-                author: "Herman Melville".to_string(),
+                title: "i为i为i开".to_string(),
+                author: "Edward".to_string(),
             },
         ]
     }
