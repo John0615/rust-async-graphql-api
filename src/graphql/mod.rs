615dd7ddfa9f4fac3fb7c3f7fe588f024526ed18
John <john.zhao@scrumcn.com>
2020-08-12 22:14:57 +0800

    调整结构

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,19 +1,8 @@
 pub use crate::graphql::schemas::query::Query;
-use actix_web::{web, HttpResponse, Result};
-use async_graphql::http::playground_source;
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
-use async_graphql_actix_web::{GQLRequest, GQLResponseStream};
+
+pub use async_graphql::http::playground_source;
+pub use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+pub use async_graphql_actix_web::{GQLRequest, GQLResponseStream};
 mod schemas;
 
-type DeferSchema = Schema<Query, EmptyMutation, EmptySubscription>;
-
-
-pub async fn index(schema: web::Data<DeferSchema>, req: GQLRequest) -> GQLResponseStream {
-    req.into_inner().execute_stream(&schema).await.into()
-}
-
-pub async fn index_playground() -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(playground_source("/", Some("/"))))
-}
+pub type DeferSchema = Schema<Query, EmptyMutation, EmptySubscription>;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use actix_cors::Cors;
-use actix_web::{guard, web, App, HttpServer};
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use actix_web::{ guard, web, App, HttpServer, Result, HttpResponse};
+use graphql::{ playground_source, DeferSchema, GQLRequest, GQLResponseStream, Query, EmptyMutation, EmptySubscription, Schema};
 
 mod graphql;
 
-use graphql::{index, index_playground, Query};
+pub async fn index(schema: web::Data<DeferSchema>, req: GQLRequest) -> GQLResponseStream {
+    req.into_inner().execute_stream(&schema).await.into()
+}
+
+pub async fn index_playground() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source("/", Some("/"))))
+}
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
